ecd811defe41e995cbff9ee99750dcac45051c9b
Dana Pughakoff <danainjax@gmail.com>
2020-08-06 13:04:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
