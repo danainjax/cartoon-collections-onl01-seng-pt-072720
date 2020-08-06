8f244511ce3f2c4da2d2bd2df0bdf3ac7a79946a
Dana Pughakoff <danainjax@gmail.com>
2020-08-06 13:07:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |type|
-    chesse_types.include?(type)
+    cheese_types.include?(type)
   end
 end
